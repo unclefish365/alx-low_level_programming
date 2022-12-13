ba7e966c090bf8825ca830e1cd747a40735d60b4
unclefish365 <rangahove@gmail.com>
2022-12-13 07:25:48 -0800

    Functions Nested Loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * File: main.h
- * Description: Header file containing declarattions for all functions
- * used in the 0x02-functions_nested_loops directory
- */
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
