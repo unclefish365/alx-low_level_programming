7e7ec3ad61d57bb6ea01782bb6ff9d1cc2c6e0ea
unclefish365 <rangahove@gmail.com>
2022-12-13 07:44:22 -0800

    Functions Nested Loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * Description: prints _putchar, followed by a new line.
+ * main - main block
+ * Description: main prints _putchar, followed by a new line.
  * Return: Always 0
  */
 int main(void)
