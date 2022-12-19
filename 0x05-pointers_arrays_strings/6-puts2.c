c779961587d6d30187cf56b1f0a808cf6fa77444
unclefish365 <rangahove@gmail.com>
2022-12-19 02:30:03 -0800

    Pointers Arrays Strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,6 +7,7 @@
 void puts2(char *str)
 {
 	int index = 0, len = 0;
+
 	while (str[index++])
 		len++;
 
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _strcpy - copies a string pointed to by @src, including the terminating null byte
+ * _strcpy - copies a string pointed to by @src,
+ * including the terminating null byte
  * to a buffer pointed to by @dest
  * @dest: a buffer to copy the string to
  * @src: the source string to copy
