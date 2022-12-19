e2e81a2899c1443b4d13c8689195441695a29f2c
unclefish365 <rangahove@gmail.com>
2022-12-19 07:10:44 -0800

    Pointers Arrays Strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,16 +3,21 @@
 /**
  * print_rev - prints a string in reverse
  * @s: the string to be printed
+ * 
+ * Return: no return
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int count = 0;
 
-	while (s[index++])
-		len++;
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
 	_putchar('\n');
 }
