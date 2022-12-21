873e8686ca5ac140b89b25244a42dff18c0ba942
unclefish365 <rangahove@gmail.com>
2022-12-21 10:26:36 -0800

    More Pointers Arrays Strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,19 +5,27 @@
  * inputted number of bytes from @src
  * @dest: the string to be appended upon
  * @src: the string to be appended to @dest
- * @n: some member
+ * @n: input value
  *
  * Return: a pointer to the resulting string @dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int i;
+	int j;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
