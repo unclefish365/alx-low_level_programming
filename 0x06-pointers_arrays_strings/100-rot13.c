e5e4a922f29e72040882977ff702e9365575e70e
unclefish365 <rangahove@gmail.com>
2022-12-21 11:17:40 -0800

    More Pointers Arrays Strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,11 +2,11 @@
 
 /**
  * rot13 - encodes a string using rot13
- * @str: the string to be encoded
+ * @s: the string to be encoded
  *
- * Reurn: a pointer to the encoded string
+ * Return: a pointer to the encoded string
  */
-char *rot13(char *str)
+char *rot13(char *s)
 {
 	int i;
 	int j;
